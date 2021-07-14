68383581094fb6f8f0628b8b0ed3394f10908100
Brandon Fadairo <BrandonFadairo@hotmail.com>
2021-07-14 19:17:15 -0400

    Beginning work on login fragment

--- starter/app/src/main/java/com/example/android/politicalpreparedness/login/LoginFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/login/LoginFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
 
@@ -15,18 +18,27 @@ class LoginFragment : Fragment() {
     }
 
     private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+        binding.loginButton.setOnClickListener { navigateToLaunchScreen() }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun navigateToLaunchScreen() {
+        findNavController().navigate(
+            LoginFragmentDirections.actionLoginFragmentToLaunchFragment()
+        )
     }
 
 }
